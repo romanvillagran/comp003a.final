d7643665d4a290bb651c349c3756c0d767dbb6a3
Your Name <you@example.com>
2025-03-02 13:21:52 -0800

    added a way to add members

---
+++ comp003a.final/Member.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp003a.final
+{
+    internal class Member
+    {
+        /// <summary>
+        /// the name of the member 
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// the age of the member 
+        /// </summary>
+        public int age;
+        /// <summary>
+        /// the plan of the member (basic/premium)
+        /// </summary>
+        public string plan;
+
+        /// <summary>
+        /// create a new member
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Age"></param>
+        /// <param name="Plan"></param>
+        public Member(string Name, int Age, string Plan)
+        {
+            Name = name;
+            Age = age;
+            Plan = plan;
+        }
+        /// <summary>
+        /// displays member details
+        /// </summary>
+        public void display()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}, Plan: {plan}");
+        }
+
+
+    }
+}
